23f6cd7ca5aceb1a2f01a0e13b7497d98cf07233
remax21 <remid@bam.tech>
2023-07-21 18:13:45 +0200

    fix(pubspec): bump versions in pubspec to get flutter map to 5.0.0

--- example/lib/screens/main/pages/settingsAndAbout/settings_and_about.dart
+++ example/lib/screens/main/pages/settingsAndAbout/settings_and_about.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import '../../../../shared/components/loading_indicator.dart';
 import 'components/header.dart';
 
@@ -96,9 +97,9 @@ class _SettingsAndAboutPageState extends State<SettingsAndAboutPage> {
                 Expanded(
                   child: SingleChildScrollView(
                     controller: creditsScrollController,
-                    child: Column(
+                    child: const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "An example application for the 'flutter_map_tile_caching' project, built by Luka S (JaffaKetchup). Tap on the above button to show more detailed information.\n",
                         ),
